204a1de7d359c3904ece5ab4b1ff1082b18eb094
rahmanhidayat1611 <rahmanhidayat1611@gmail.com>
2023-01-11 09:02:57 +0700

    push

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -134,7 +134,7 @@
                                             <svg class="bi text-primary" width="32" height="32" fill="blue"
                                                 style="width:10px">
                                                 <use
-                                                    xlink:href="assets/images/bootstrap-icons.svg#circle-fill" />
+                                                    xlink:href="{{ asset('mazer') }}/assets/images/bootstrap-icons.svg#circle-fill" />
                                             </svg>
                                             <h5 class="mb-0 ms-3">Europe</h5>
                                         </div>
@@ -152,7 +152,7 @@
                                             <svg class="bi text-success" width="32" height="32" fill="blue"
                                                 style="width:10px">
                                                 <use
-                                                    xlink:href="assets/images/bootstrap-icons.svg#circle-fill" />
+                                                    xlink:href="{{ asset('mazer') }}/assets/images/bootstrap-icons.svg#circle-fill" />
                                             </svg>
                                             <h5 class="mb-0 ms-3">America</h5>
                                         </div>
@@ -170,7 +170,7 @@
                                             <svg class="bi text-danger" width="32" height="32" fill="blue"
                                                 style="width:10px">
                                                 <use
-                                                    xlink:href="assets/images/bootstrap-icons.svg#circle-fill" />
+                                                    xlink:href="{{ asset('mazer') }}/assets/images/bootstrap-icons.svg#circle-fill" />
                                             </svg>
                                             <h5 class="mb-0 ms-3">Indonesia</h5>
                                         </div>
@@ -204,7 +204,7 @@
                                                 <td class="col-3">
                                                     <div class="d-flex align-items-center">
                                                         <div class="avatar avatar-md">
-                                                            <img src="assets/images/faces/5.jpg">
+                                                            <img src="{{ asset('mazer') }}/assets/images/faces/5.jpg">
                                                         </div>
                                                         <p class="font-bold ms-3 mb-0">Si Cantik</p>
                                                     </div>
@@ -217,7 +217,7 @@
                                                 <td class="col-3">
                                                     <div class="d-flex align-items-center">
                                                         <div class="avatar avatar-md">
-                                                            <img src="assets/images/faces/2.jpg">
+                                                            <img src="{{ asset('mazer') }}/assets/images/faces/2.jpg">
                                                         </div>
                                                         <p class="font-bold ms-3 mb-0">Si Ganteng</p>
                                                     </div>
@@ -240,7 +240,7 @@
                     <div class="card-body py-4 px-5">
                         <div class="d-flex align-items-center">
                             <div class="avatar avatar-xl">
-                                <img src="assets/images/faces/1.jpg" alt="Face 1">
+                                <img src="{{ asset('mazer') }}/assets/images/faces/1.jpg" alt="Face 1">
                             </div>
                             <div class="ms-3 name">
                                 <h5 class="font-bold">John Duck</h5>
@@ -256,7 +256,7 @@
                     <div class="card-content pb-4">
                         <div class="recent-message d-flex px-4 py-3">
                             <div class="avatar avatar-lg">
-                                <img src="assets/images/faces/4.jpg">
+                                <img src="{{ asset('mazer') }}/assets/images/faces/4.jpg">
                             </div>
                             <div class="name ms-4">
                                 <h5 class="mb-1">Hank Schrader</h5>
@@ -265,7 +265,7 @@
                         </div>
                         <div class="recent-message d-flex px-4 py-3">
                             <div class="avatar avatar-lg">
-                                <img src="assets/images/faces/5.jpg">
+                                <img src="{{ asset('mazer') }}/assets/images/faces/5.jpg">
                             </div>
                             <div class="name ms-4">
                                 <h5 class="mb-1">Dean Winchester</h5>
@@ -274,7 +274,7 @@
                         </div>
                         <div class="recent-message d-flex px-4 py-3">
                             <div class="avatar avatar-lg">
-                                <img src="assets/images/faces/1.jpg">
+                                <img src="{{ asset('mazer') }}/assets/images/faces/1.jpg">
                             </div>
                             <div class="name ms-4">
                                 <h5 class="mb-1">John Dodol</h5>
